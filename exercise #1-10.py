5b8bde9305f3a78dcbb231355805b0c43113d225
Patrykbartczak <patryk.bartczak99@gmail.com>
2025-11-29 12:57:35 +0100

    Exercise

---
+++ exercise #1-10.py
@@ -0,0 +1,17 @@
+            # 1. Hello World!
+# print('Hello World')
+# print("What's your name: ?")
+# my_name = input()
+# print(f"Hello {my_name}")
+            # 2. Temp conversion
+def convertofahrenheit(cel):
+    #Calculate and return the degrees Fahrenheit:
+    return cel * (9/5) + 32
+
+def converttocelsius(fah):
+    #Calculate and return the degrees celsius:
+    return (fah - 32) * (5/9)
+
+# print(converttocelsius(5))
+# print(convertofahrenheit(30))
+
